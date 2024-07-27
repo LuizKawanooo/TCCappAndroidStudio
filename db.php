ed888da13b2ca3bbcdef59698d1fb9c1d495856a
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-07-27 16:45:02 -0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "tccappionic-bd.mysql.uhserver.com";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
+$dbname = "tccappionic_bd";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
