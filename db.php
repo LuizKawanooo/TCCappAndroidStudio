c794343e25e27db973cb39fce5ff2f42f4fd39f8
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-07-27 17:01:39 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,13 @@
 <?php
-$servername = "tccappionic-bd.mysql.uhserver.com";
-$username = "ionic_perfil_bd";
-$password = "{[UOLluiz2019";
-$dbname = "tccappionic_bd";
+$servername = "tccappionic-bd.mysql.uhserver.com"; // Ou o nome do seu servidor
+$username = "ionic_perfil_bd"; // Seu usuário do MySQL
+$password = "{[UOLluiz2019"; // Sua senha do MySQL
+$dbname = "tccappionic_bd"; // Nome do seu banco de dados
 
+// Crie a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifique a conexão
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
