9dc3f43c84b788c73a5f83194eadd5e750aea303
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-07-28 13:27:39 -0300

    Update listar_usuarios.php

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -2,10 +2,10 @@
 header('Content-Type: application/json');
 
 // Configurações do banco de dados
-$host = 'localhost';
-$db   = 'nome_do_banco';
-$user = 'usuario';
-$pass = 'senha';
+$host = 'tccappionic-bd.mysql.uhserver.com';
+$db   = 'tccappionic_bd';
+$user = 'ionic_perfil_bd';
+$pass = '{[UOLluiz2019';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
