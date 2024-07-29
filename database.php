b2703de8a185105cba8d315883e79252a7303d85
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-07-29 09:36:02 -0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'tccappionic-bd.mysql.uhserver.com';
+$db   = 'tccappionic_bd';
+$user = 'ionic_perfil_bd';
+$pass = '{[UOLluiz2019';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    // Definir o modo de erro do PDO para exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit();
+}
+?>
