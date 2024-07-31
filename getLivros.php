34b26dc1053ad18ea3d8a05cce30f57a69c1e11c
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-07-31 14:58:50 -0300

    Update getLivros.php

--- getLivros.php
+++ getLivros.php
@@ -2,17 +2,17 @@
 header("Content-Type: application/json");
 
 // Conectar ao banco de dados
-$host = "localhost";
-$dbname = "seu_banco_de_dados";
-$username = "seu_usuario";
-$password = "sua_senha";
+$host = "tccappionic-bd.mysql.uhserver.com";
+$dbname = "tccappionic_bd";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Preparar a consulta
-    $stmt = $pdo->prepare("SELECT titulo, genero, autor, editora, tombo, ano, classificacao, n_paginas, isbn, imagem FROM livros");
+    $stmt = $pdo->prepare("SELECT titulo, genero, autor, editora, tombo, ano, classificacao, n_paginas, isbn, imagem FROM livro");
     $stmt->execute();
 
     // Buscar todos os livros
