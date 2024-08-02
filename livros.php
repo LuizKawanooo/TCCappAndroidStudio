ec739e709b55893f9b3ec1889742bf3b0a1196b5
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-02 10:20:26 -0300

    Update livros.php

--- livros.php
+++ livros.php
@@ -1,36 +1,30 @@
 <?php
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
-// Configurações do banco de dados
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
-// Cria conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
 if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// Consulta SQL para obter os livros
-$sql = "SELECT id, titulo, autor, imagem FROM livros";
+$sql = "SELECT titulo, genero, autor, editora, tombo, ano, classificacao, n_paginas, isbn, imagem FROM livros";
 $result = $conn->query($sql);
 
 $livros = array();
 
 if ($result->num_rows > 0) {
-    // Adiciona cada linha do resultado ao array
     while($row = $result->fetch_assoc()) {
         $livros[] = $row;
     }
 }
 
-// Converte o array para JSON e imprime
-echo json_encode($livros);
-
-// Fecha a conexão
 $conn->close();
+
+echo json_encode($livros);
 ?>
