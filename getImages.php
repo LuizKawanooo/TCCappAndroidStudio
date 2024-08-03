1cb76f41bfd6f38eeea74807c257dd21ba085190
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-03 17:14:55 -0300

    Update getImages.php

--- getImages.php
+++ getImages.php
@@ -1,8 +1,14 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept');
+
 
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
