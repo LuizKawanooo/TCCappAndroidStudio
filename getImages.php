3b857881bb9e473cca1c981a266f60edad7dca48
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-03 14:43:39 -0300

    Create getImages.php

---
+++ getImages.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept');
+
+$servername = "tccappionic-bd.mysql.uhserver.com";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
+$dbname = "tccappionic_bd";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, imagem FROM livros"; // Substitua "images" pelo nome da sua tabela
+$result = $conn->query($sql);
+
+$images = [];
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $images[] = [
+            'id' => $row['id'],
+            'image_url' => 'data:image/jpeg;base64,' . base64_encode($row['image_blob'])
+        ];
+    }
+}
+
+echo json_encode($images);
+
+$conn->close();
+?>
