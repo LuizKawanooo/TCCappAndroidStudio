21e367335f3fcdec686a1b0b4491d8ed0920b05d
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-03 16:42:27 -0300

    Update getImages.php

--- getImages.php
+++ getImages.php
@@ -1,3 +1,42 @@
+// <?php
+// header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+// header('Access-Control-Allow-Headers: Origin, Content-Type, Accept');
+
+// $servername = "tccappionic-bd.mysql.uhserver.com";
+// $username = "ionic_perfil_bd";
+// $password = "{[UOLluiz2019";
+// $dbname = "tccappionic_bd";
+
+// // Cria a conexão
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Verifica a conexão
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sql = "SELECT id, imagem FROM livros"; // Substitua "images" pelo nome da sua tabela
+// $result = $conn->query($sql);
+
+// $images = [];
+
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         $images[] = [
+//             'id' => $row['id'],
+//             'image_url' => 'data:image/jpeg;base64,' . base64_encode($row['imagem'])
+//         ];
+//     }
+// }
+
+// echo json_encode($images);
+
+// $conn->close();
+// ?>
+
+
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
@@ -17,7 +56,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, imagem FROM livros"; // Substitua "images" pelo nome da sua tabela
+$sql = "SELECT id, imagem, imagem_status FROM livros"; // Inclua o imagem_status
 $result = $conn->query($sql);
 
 $images = [];
@@ -26,7 +65,8 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $images[] = [
             'id' => $row['id'],
-            'image_url' => 'data:image/jpeg;base64,' . base64_encode($row['imagem'])
+            'image_url' => 'data:image/jpeg;base64,' . base64_encode($row['imagem']),
+            'status' => $row['imagem_status'] // Inclua o imagem_status
         ];
     }
 }
