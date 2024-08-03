890660aa1747eeeba40618400ca8e77e08f1b6cc
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-03 16:12:26 -0300

    Update getImages.php

--- getImages.php
+++ getImages.php
@@ -1,42 +1,42 @@
-// <?php
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-// header('Access-Control-Allow-Headers: Origin, Content-Type, Accept');
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept');
 
-// $servername = "tccappionic-bd.mysql.uhserver.com";
-// $username = "ionic_perfil_bd";
-// $password = "{[UOLluiz2019";
-// $dbname = "tccappionic_bd";
+$servername = "tccappionic-bd.mysql.uhserver.com";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
+$dbname = "tccappionic_bd";
 
-// // Cria a conexão
-// $conn = new mysqli($servername, $username, $password, $dbname);
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// // Verifica a conexão
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-// $sql = "SELECT id, imagem FROM livros"; // Substitua "images" pelo nome da sua tabela
-// $result = $conn->query($sql);
+$sql = "SELECT id, imagem FROM livros"; // Substitua "images" pelo nome da sua tabela
+$result = $conn->query($sql);
 
-// $images = [];
+$images = [];
 
-// if ($result->num_rows > 0) {
-//     while($row = $result->fetch_assoc()) {
-//         $images[] = [
-//             'id' => $row['id'],
-//             'image_url' => 'data:image/jpeg;base64,' . base64_encode($row['imagem'])
-//         ];
-//     }
-// }
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $images[] = [
+            'id' => $row['id'],
+            'image_url' => 'data:image/jpeg;base64,' . base64_encode($row['imagem'])
+        ];
+    }
+}
 
-// echo json_encode($images);
+echo json_encode($images);
 
-// $conn->close();
-// ?>
+$conn->close();
+?>
 
-
+<!-- 
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
@@ -74,3 +74,4 @@ echo json_encode($images);
 
 $conn->close();
 ?>
+ -->
