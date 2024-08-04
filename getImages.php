5091f15059d52a34b5e41979f25dd46287187f78
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-03 22:23:12 -0300

    Update getImages.php

--- getImages.php
+++ getImages.php
@@ -9,36 +9,29 @@ $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to fetch images and status_livro
-$sql = "SELECT id, imagem, status_livro FROM livros"; // Supondo que a tabela de imagens tenha colunas 'id', 'imagem' e 'status_livro'
+$sql = "SELECT id, imagem, status_livros FROM livros";
 $result = $conn->query($sql);
 
 $images = array();
 
 if ($result->num_rows > 0) {
-    // Output data of each row
     while($row = $result->fetch_assoc()) {
         $images[] = array(
             "id" => $row["id"],
-            "image_url" => 'data:image/jpeg;base64,' . base64_encode($row["imagem"]), // Converter BLOB para base64 e criar uma URL de dados
-            "status_livro" => $row["status_livro"]
+            "image_url" => 'data:image/jpeg;base64,' . base64_encode($row["imagem"]),
+            "status_livros" => $row["status_livros"]
         );
     }
+    echo json_encode(array("images" => $images));
 } else {
     echo json_encode(array("message" => "No images found"));
-    $conn->close();
-    exit();
 }
 
 $conn->close();
-
-echo json_encode(array("images" => $images));
 ?>
