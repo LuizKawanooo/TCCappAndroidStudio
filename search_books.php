04bedcf3810db9014efe6a5d085d8e4696c4a110
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-05 09:36:59 -0300

    Update search_books.php

--- search_books.php
+++ search_books.php
@@ -1,30 +1,49 @@
 <?php
-header('Content-Type: application/json');
-require 'db_connection.php'; // Ajuste o caminho conforme necessário
+$servername = "tccappionic-bd.mysql.uhserver.com";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
+$dbname = "tccappionic_bd";
+
+// Conectar ao banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
 
 $searchTerm = isset($_GET['q']) ? '%' . $_GET['q'] . '%' : '%';
 
-// Prepara e executa a consulta SQL
-$sql = "SELECT `id`, `titulo` AS `title`, `genero`, `autor` AS `author`, `editora`, `tombo`, `ano`, `classificacao`, `n_paginas`, `isbn`, `sinopse`, `image_url`, `status_livros`
-        FROM `livros`
+// Preparar a consulta
+$sql = "SELECT `id`, `titulo`, `genero`, `autor`, `editora`, `tombo`, `ano`, `classificacao`, `n_paginas`, `isbn`, `sinopse`, `image_url`, `status_livros` 
+        FROM livros 
         WHERE `titulo` LIKE ? OR `autor` LIKE ?";
 
 $stmt = $conn->prepare($sql);
-$stmt->bind_param('ss', $searchTerm, $searchTerm);
 
-if ($stmt->execute()) {
-    $result = $stmt->get_result();
-    $books = [];
-
-    while ($row = $result->fetch_assoc()) {
-        $books[] = $row;
-    }
-
-    echo json_encode($books);
-} else {
-    echo json_encode([]);
+if ($stmt === false) {
+    die("Erro na preparação da consulta: " . $conn->error);
 }
 
+// Bind dos parâmetros
+$stmt->bind_param('ss', $searchTerm, $searchTerm);
+
+// Executar a consulta
+$stmt->execute();
+
+// Obter resultados
+$result = $stmt->get_result();
+
+$books = array();
+while ($row = $result->fetch_assoc()) {
+    $books[] = $row;
+}
+
+// Retornar resultados como JSON
+header('Content-Type: application/json');
+echo json_encode($books);
+
+// Fechar a conexão
 $stmt->close();
 $conn->close();
 ?>
