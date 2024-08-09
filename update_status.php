245524673122f0575f95d4abf91932c0b650bb87
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-09 15:21:10 -0300

    Update update_status.php

--- update_status.php
+++ update_status.php
@@ -42,13 +42,13 @@
 
 
 
+
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); // Permite solicitações de qualquer origem
-header('Access-Control-Allow-Methods: POST'); // Permite apenas o método POST
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Conectar ao banco de dados
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
@@ -56,7 +56,6 @@ $dbname = "tccappionic_bd";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar a conexão
 if ($conn->connect_error) {
     echo json_encode(['message' => 'Connection failed: ' . $conn->connect_error]);
     exit;
@@ -64,20 +63,18 @@ if ($conn->connect_error) {
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-// Verificar se a entrada JSON é válida
 if (json_last_error() !== JSON_ERROR_NONE) {
     echo json_encode(['message' => 'Invalid JSON input']);
     $conn->close();
     exit;
 }
 
-// Verificar se os dados esperados estão presentes
 if (isset($data['id']) && isset($data['status'])) {
     $id = intval($data['id']);
     $status = intval($data['status']);
     
     if ($status === 1) {
-        $rental_end_time = date('Y-m-d H:i:s', time() + 30); // Define o tempo de expiração para 30 segundos no futuro
+        $rental_end_time = date('Y-m-d H:i:s', time() + 30); // Tempo de expiração para 30 segundos no futuro
         $update_sql = "UPDATE livros SET status_livros = ?, rental_end_time = ? WHERE id = ?";
         $stmt_update = $conn->prepare($update_sql);
         if ($stmt_update === false) {
@@ -110,4 +107,3 @@ if (isset($data['id']) && isset($data['status'])) {
 
 $conn->close();
 ?>
-
