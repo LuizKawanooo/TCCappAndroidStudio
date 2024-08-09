55b608a9612dcf4f012f91f7057819427dad7522
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-09 00:15:40 -0300

    Update get_status.php

--- get_status.php
+++ get_status.php
@@ -1,49 +1,7 @@
-// <?php
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET');
-// header('Access-Control-Allow-Headers: Content-Type');
-
-// $servername = "tccappionic-bd.mysql.uhserver.com";
-// $username = "ionic_perfil_bd";
-// $password = "{[UOLluiz2019";
-// $dbname = "tccappionic_bd";
-
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// $id = intval($_GET['id']);
-
-// $sql = "SELECT status_livros FROM livros WHERE id = ?";
-// $stmt = $conn->prepare($sql);
-// $stmt->bind_param("i", $id);
-// $stmt->execute();
-// $stmt->bind_result($status_livros);
-// $stmt->fetch();
-// $stmt->close();
-
-// echo json_encode(['status_livros' => $status_livros]);
-
-// $conn->close();
-// ?>
-
-
-
-
-
-
-
-
-
-
-
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Content-Type');
 
 $servername = "tccappionic-bd.mysql.uhserver.com";
@@ -57,20 +15,18 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$id = $_GET['id'];
-$sql = "SELECT status_livros, rental_end_time FROM livros WHERE id = ?";
+$id = intval($_GET['id']);
+
+$sql = "SELECT status_livros FROM livros WHERE id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $id);
 $stmt->execute();
-$result = $stmt->get_result();
-$book = $result->fetch_assoc();
-
-if ($book) {
-    echo json_encode($book);
-} else {
-    echo json_encode(array("message" => "Book not found"));
-}
-
+$stmt->bind_result($status_livros);
+$stmt->fetch();
 $stmt->close();
+
+echo json_encode(['status_livros' => $status_livros]);
+
 $conn->close();
 ?>
+
