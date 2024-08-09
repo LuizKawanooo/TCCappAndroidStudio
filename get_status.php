4fa0824963456fac378387fdcc3d53c3d8a2f564
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-08 23:56:33 -0300

    Create get_status.php

---
+++ get_status.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type');
+
+$servername = "tccappionic-bd.mysql.uhserver.com";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
+$dbname = "tccappionic_bd";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$id = intval($_GET['id']);
+
+$sql = "SELECT status_livros FROM livros WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->bind_result($status_livros);
+$stmt->fetch();
+$stmt->close();
+
+echo json_encode(['status_livros' => $status_livros]);
+
+$conn->close();
+?>
