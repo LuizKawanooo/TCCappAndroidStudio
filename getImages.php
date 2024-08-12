9f87c2cbaac1e0f918bc3a84b72be9dc72f4cea5
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-12 15:04:24 -0300

    Update getImages.php

--- getImages.php
+++ getImages.php
@@ -1,11 +1,9 @@
 <?php
-// Configurar cabeçalhos para resposta JSON e CORS
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Configurações de conexão com o banco de dados
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
@@ -19,9 +17,10 @@ if ($conn->connect_error) {
     die(json_encode(array("error" => "Falha na conexão com o banco de dados: " . $conn->connect_error)));
 }
 
-// Verificar se o parâmetro 'genre', 'search' ou 'searchTerm' foi fornecido
+// Verificar se o parâmetro 'genre', 'search' ou 'title' foi fornecido
 $genre = isset($_GET['genre']) ? $conn->real_escape_string($_GET['genre']) : '';
 $searchTerm = isset($_GET['search']) ? $conn->real_escape_string($_GET['search']) : '';
+$title = isset($_GET['title']) ? $conn->real_escape_string($_GET['title']) : '';
 
 // Preparar a consulta SQL
 $sql = "SELECT id, imagem, status_livros FROM livros";
@@ -34,11 +33,17 @@ if (!empty($genre)) {
     $params[] = $genre;
 }
 
-// Adicionar a condição de filtro por pesquisa por autor ou título, se fornecido
+// Adicionar a condição de filtro por pesquisa, se fornecido
 if (!empty($searchTerm)) {
     $conditions[] = "(autor LIKE ? OR titulo LIKE ?)";
     $params[] = '%' . $searchTerm . '%';
     $params[] = '%' . $searchTerm . '%';
+}
+
+// Adicionar a condição de filtro por título, se fornecido
+if (!empty($title)) {
+    $conditions[] = "titulo LIKE ?";
+    $params[] = '%' . $title . '%';
 }
 
 // Adicionar condições à consulta SQL
@@ -56,10 +61,12 @@ if ($stmt === false) {
     die(json_encode(array("error" => "Falha ao preparar a consulta: " . $conn->error)));
 }
 
+// Vincular parâmetros, se houver
 if (!empty($params)) {
     $types = str_repeat('s', count($params)); // Tipo dos parâmetros
     $stmt->bind_param($types, ...$params);
 }
+
 $stmt->execute();
 $result = $stmt->get_result();
 
