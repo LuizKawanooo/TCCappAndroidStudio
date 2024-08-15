ee1e15a22b130f8c1134802e939c98abd306d45b
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-14 23:24:42 -0300

    Update get_artigos.php

--- get_artigos.php
+++ get_artigos.php
@@ -1,5 +1,4 @@
 <?php
-// Habilitar exibição de erros
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -7,25 +6,26 @@ error_reporting(E_ALL);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header('Content-Type: application/json'); // Define o tipo de conteúdo como JSON
+header('Content-Type: application/json');
 
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
-// Conectar ao banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die(json_encode(['error' => 'Conexão falhou: ' . $conn->connect_error]));
+    echo json_encode(['error' => 'Conexão falhou: ' . $conn->connect_error]);
+    exit;
 }
 
 $sql = "SELECT id, titulo, descricao, pdf_nome FROM artigos";
 $result = $conn->query($sql);
 
 if (!$result) {
-    die(json_encode(['error' => 'Erro na consulta SQL: ' . $conn->error]));
+    echo json_encode(['error' => 'Erro na consulta SQL: ' . $conn->error]);
+    exit;
 }
 
 $artigos = [];
@@ -35,9 +35,6 @@ if ($result->num_rows > 0) {
         $artigos[] = $row;
     }
 }
-
-// Mensagem de depuração
-// echo 'Dados retornados: ' . print_r($artigos, true);
 
 echo json_encode(['artigos' => $artigos]);
 
