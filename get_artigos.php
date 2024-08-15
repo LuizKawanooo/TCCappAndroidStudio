075874e216fde733cd362de4b78a3eba816e3654
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-14 23:10:05 -0300

    Update get_artigos.php

--- get_artigos.php
+++ get_artigos.php
@@ -10,31 +10,32 @@ $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
-// Conecta ao banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Executa a consulta SQL
-$sql = "SELECT id, titulo, descricao, pdf_nome, arquivo FROM artigos";
-$result = $conn->query($sql);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-$artigos = array();
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $artigos[] = $row;
-    }
+    $stmt = $conn->prepare("SELECT pdf_nome, arquivo FROM artigos WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($pdf_nome, $arquivo);
+    $stmt->fetch();
+
+    // Define o tipo de conteúdo como PDF
+    header("Content-Type: application/pdf");
+    header("Content-Disposition: inline; filename=\"$pdf_nome\"");
+    header("Content-Length: " . strlen($arquivo));
+
+    // Exibe o conteúdo PDF
+    echo $arquivo;
+
+    $stmt->close();
 }
 
-// Define o cabeçalho como JSON
-header('Content-Type: application/json');
-
-// Retorna os dados em formato JSON
-echo json_encode(array('artigos' => $artigos));
-
-// Fecha a conexão
 $conn->close();
 ?>
