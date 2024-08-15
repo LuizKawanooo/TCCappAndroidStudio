438eb5536a213d2eba762b1a28a14c9467e3ac95
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-14 23:08:05 -0300

    Update get_artigos.php

--- get_artigos.php
+++ get_artigos.php
@@ -4,17 +4,21 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
+
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
+// Conecta ao banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifica a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
+// Executa a consulta SQL
 $sql = "SELECT id, titulo, descricao, pdf_nome, arquivo FROM artigos";
 $result = $conn->query($sql);
 
@@ -25,9 +29,12 @@ if ($result->num_rows > 0) {
     }
 }
 
+// Define o cabeçalho como JSON
 header('Content-Type: application/json');
+
+// Retorna os dados em formato JSON
 echo json_encode(array('artigos' => $artigos));
 
+// Fecha a conexão
 $conn->close();
 ?>
-
