7e57149be00e496369a8ebf169453067e5b1bf92
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-14 23:14:56 -0300

    Update get_artigos.php

--- get_artigos.php
+++ get_artigos.php
@@ -4,33 +4,38 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json'); // Define o tipo de conteúdo como JSON
 
+
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
+// Criar a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificar a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Consulta para obter todos os artigos
+// Consulta para buscar artigos
 $sql = "SELECT id, titulo, descricao, pdf_nome FROM artigos";
 $result = $conn->query($sql);
 
+// Inicializar um array para armazenar os artigos
 $artigos = [];
+
 if ($result->num_rows > 0) {
-    // Preenche o array com os resultados
-    while ($row = $result->fetch_assoc()) {
+    // Busca cada linha e armazena no array
+    while($row = $result->fetch_assoc()) {
         $artigos[] = $row;
     }
-} else {
-    echo json_encode(["artigos" => []]);
-    exit();
 }
 
-echo json_encode(["artigos" => $artigos]);
-
+// Fechar a conexão
 $conn->close();
+
+// Enviar a resposta JSON
+echo json_encode(['artigos' => $artigos]);
 ?>
+
