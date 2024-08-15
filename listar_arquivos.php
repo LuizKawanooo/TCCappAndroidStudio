8b6c1e7aaef9635d11ed5985b30bacbc3259d379
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-15 10:46:24 -0300

    Update listar_arquivos.php

--- listar_arquivos.php
+++ listar_arquivos.php
@@ -1,36 +1,17 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Configurações do banco de dados
 $dsn = 'mysql:host=tccappionic-bd.mysql.uhserver.com;dbname=tccappionic_bd';
 $username = 'ionic_perfil_bd';
 $password = '{[UOLluiz2019';
 
-// Criar conexão com o banco de dados
 try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem-sucedida!";
 } catch (PDOException $e) {
-    echo json_encode(array("error" => "Conexão falhou: " . $e->getMessage()));
-    exit();
+    echo "Falha na conexão: " . $e->getMessage();
 }
-
-// Preparar e executar a consulta SQL
-$sql = "SELECT id, titulo, descricao, pdf_nome FROM artigos";
-
-try {
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $artigos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    echo json_encode(array("artigos" => $artigos));
-} catch (PDOException $e) {
-    echo json_encode(array("error" => "Erro na consulta: " . $e->getMessage()));
-}
-
-// Fechar a conexão
-$pdo = null;
 ?>
