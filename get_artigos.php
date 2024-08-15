3703561f6dffec6fc5601242a06f8bdf66bf6566
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-14 23:05:41 -0300

    Update get_artigos.php

--- get_artigos.php
+++ get_artigos.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
@@ -14,21 +15,19 @@ if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Consulta para buscar todos os artigos
-$sql = "SELECT id, titulo, descricao, pdf_nome FROM artigos";
+$sql = "SELECT id, titulo, descricao, pdf_nome, arquivo FROM artigos";
 $result = $conn->query($sql);
 
-$artigos = [];
-
+$artigos = array();
 if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+    while($row = $result->fetch_assoc()) {
         $artigos[] = $row;
     }
 }
 
-// Retorna os artigos em formato JSON
 header('Content-Type: application/json');
-echo json_encode(['artigos' => $artigos]);
+echo json_encode(array('artigos' => $artigos));
 
 $conn->close();
 ?>
+
