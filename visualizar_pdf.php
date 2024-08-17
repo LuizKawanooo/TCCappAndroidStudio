adad7901b22d201ea56cfcfbc0aac7c8cb114c0c
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-17 14:33:56 -0300

    Create visualizar_pdf.php

---
+++ visualizar_pdf.php
@@ -0,0 +1,44 @@
+<?php
+function verificarToken($id, $token) {
+    $chave = 'sua_chave_secreta'; // Deve ser a mesma chave secreta usada para gerar o token
+    return hash_hmac('sha256', $id, $chave) === $token;
+}
+
+$host = 'tccappionic-bd.mysql.uhserver.com';
+$user = 'ionic_perfil_bd';
+$password = '{[UOLluiz2019';
+$database = 'tccappionic_bd';
+
+$mysqli = new mysqli($host, $user, $password, $database);
+
+if ($mysqli->connect_error) {
+    die("Falha na conexão: " . $mysqli->connect_error);
+}
+
+$id = $_GET['id'];
+$token = $_GET['token'];
+
+if (verificarToken($id, $token)) {
+    $query = "SELECT arquivo, pdf_name FROM artigos WHERE id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($arquivo, $pdf_nome);
+    $stmt->fetch();
+
+    if ($stmt->num_rows > 0) {
+        header("Content-Type: application/pdf");
+        header("Content-Disposition: inline; filename=\"" . $pdf_nome . "\"");
+        echo $arquivo;
+    } else {
+        echo "PDF não encontrado.";
+    }
+
+    $stmt->close();
+} else {
+    echo "Token inválido.";
+}
+
+$mysqli->close();
+?>
