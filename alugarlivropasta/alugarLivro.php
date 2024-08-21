a0e02b2bde632a58aad2b526cf67ef6ce6a6113b
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-21 14:08:03 -0300

    Update alugarLivro.php

--- alugarlivropasta/alugarLivro.php
+++ alugarlivropasta/alugarLivro.php
@@ -1,25 +1,19 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); // Permitir todas as origens
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); // Métodos permitidos
-header('Access-Control-Allow-Headers: Content-Type'); // Cabeçalhos permitidos
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
 
-// Verificar se é uma requisição OPTIONS e sair se for
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit();
-}
-
-
-
-// Conexão com o banco de dados
+// Configuração do banco de dados
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
+// Conectar ao banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar a conexão
+// Verificar conexão
 if ($conn->connect_error) {
     echo json_encode(["success" => false, "message" => "Conexão falhou: " . $conn->connect_error]);
     exit();
@@ -27,6 +21,12 @@ if ($conn->connect_error) {
 
 // Receber dados da requisição
 $data = json_decode(file_get_contents("php://input"), true);
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo json_encode(["success" => false, "message" => "Erro ao decodificar JSON"]);
+    $conn->close();
+    exit();
+}
+
 $id = isset($data['id']) ? intval($data['id']) : 0;
 $data_aluguel = isset($data['data_aluguel']) ? date('Y-m-d H:i:s', intval($data['data_aluguel'])) : date('Y-m-d H:i:s');
 
