f04b921ef5a9b983ea0909cbaddf5aef7a6df3d4
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-21 14:14:59 -0300

    Update login.php

--- sitebibliotec2/sitetcc/login.php
+++ sitebibliotec2/sitetcc/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bibliotec - Login</title>
+    <title>Bibliotec - artigos</title>
     <link rel="shortcut icon" href="img/logo.png">
 </head>
 <body>
