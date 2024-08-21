a8a2f57a950daa553bb971f4d64c77377979a94c
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-08-21 14:41:29 -0300

    Update alugarLivro.php

--- alugarlivropasta/alugarLivro.php
+++ alugarlivropasta/alugarLivro.php
@@ -1,36 +1,35 @@
 <?php
-// Adiciona cabeçalhos CORS
-header("Access-Control-Allow-Origin: *"); // Permite qualquer origem. Substitua '*' pelo domínio específico em produção
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // Métodos permitidos
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeçalhos permitidos
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// Resto do código PHP
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
 $dbname = "tccappionic_bd";
 
+// Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar a conexão
+// Verifica a conexão
 if ($conn->connect_error) {
     echo json_encode(["success" => false, "message" => "Conexão falhou: " . $conn->connect_error]);
     exit();
 }
 
-// Receber dados da requisição
+// Recebe os dados da requisição
 $data = json_decode(file_get_contents("php://input"), true);
 $id = isset($data['id']) ? intval($data['id']) : 0;
 $data_aluguel = isset($data['data_aluguel']) ? date('Y-m-d H:i:s', intval($data['data_aluguel'])) : date('Y-m-d H:i:s');
 
-// Validar ID do livro
+// Valida o ID
 if ($id <= 0) {
     echo json_encode(["success" => false, "message" => "ID inválido"]);
     $conn->close();
     exit();
 }
 
-// Preparar e executar a consulta para verificar o status do livro
+// Prepara e executa a consulta para verificar o status do livro
 $stmt = $conn->prepare("SELECT status_livros FROM livros WHERE id = ?");
 $stmt->bind_param("i", $id);
 $stmt->execute();
@@ -39,7 +38,7 @@ $result = $stmt->get_result();
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     if ($row['status_livros'] == 0) {
-        // Preparar e executar a atualização do status do livro
+        // Prepara e executa a atualização do status do livro
         $stmt = $conn->prepare("UPDATE livros SET status_livros = 1, data_aluguel = ? WHERE id = ?");
         $stmt->bind_param("si", $data_aluguel, $id);
         if ($stmt->execute()) {
