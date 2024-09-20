6177c74a76d7b771b553cd0206bb648d507a89d5
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-09-20 17:05:37 -0300

    Create db2.php

---
+++ db2.php
@@ -0,0 +1,4 @@
+<?php
+$pdo = new PDO('mysql:host=tccappionic-bd.mysql.uhserver.com;dbname=tccappionic_bd', 'ionic_perfil_bd', '{[UOLluiz2019');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
