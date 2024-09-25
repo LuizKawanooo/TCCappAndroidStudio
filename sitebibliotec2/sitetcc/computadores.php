f276217f4c7973184901130a96056b27d3b8c0d9
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-09-25 14:53:25 -0300

    Update computadores.php

--- sitebibliotec2/sitetcc/computadores.php
+++ sitebibliotec2/sitetcc/computadores.php
@@ -17,48 +17,30 @@
 
 
 
-// Inicializa a variável de mensagem
-$mensagem = '';
 
-// Verificar se foi enviado um número de computador pelo formulário
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['numero'], $_POST['data'], $_POST['horario'], $_FILES['file'])) {
+
+    // Inicializa a variável de mensagem
+    $mensagem = '';
+
+    // Verificar se foi enviado um número de computador pelo formulário
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['numero'], $_POST['data'], $_POST['horario'])) {
         $numero = $_POST['numero'];
         $data = $_POST['data'];
         $horario = $_POST['horario'];
-        
-        // Processar a imagem
-        $file = $_FILES['file'];
 
-        // Verificar se houve erro no upload
-        if ($file['error'] == UPLOAD_ERR_OK) {
-            $imageData = file_get_contents($file['tmp_name']);
-            $imageData = base64_encode($imageData); // Opcional: se você quiser armazenar como base64
+        // Consultar o banco de dados para verificar a disponibilidade do computador
+        $sql = "SELECT * FROM computador WHERE numero = ? AND data_disponibilidade = ? AND horario_disponibilidade = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$numero, $data, $horario]);
+        $computador = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            // Consultar o banco de dados para verificar a disponibilidade do computador
-            $sql = "SELECT * FROM computador WHERE numero = ? AND data_disponibilidade = ? AND horario_disponibilidade = ?";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$numero, $data, $horario]);
-            $computador = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($computador) {
-                $mensagem = "<p style='color:#f00;'>O computador não está disponível</p>";
-            } else {
-                // Inserir a imagem e as informações do computador no banco de dados
-                $insertSql = "INSERT INTO computador (numero, data_disponibilidade, horario_disponibilidade, imagem) VALUES (?, ?, ?, ?)";
-                $insertStmt = $pdo->prepare($insertSql);
-                $insertStmt->execute([$numero, $data, $horario, $imageData]);
-                
-                $mensagem = "<p style='color:#2ACA22;'>O computador está disponível e a imagem foi carregada com sucesso.</p>";
-            }
+        if ($computador) {
+            $mensagem = "<p style='color:#f00; font-size:20px; position: absolute; top: 68%; left: 50%; transform:translate(-50%, -50%);'>O computador não está disponível</p>";
         } else {
-            $mensagem = "<p style='color:#f00;'>Erro no upload da imagem.</p>";
+            $mensagem = "<p style='color:#2ACA22; font-size:20px; position: absolute; top: 68%; left: 50%; transform:translate(-50%, -50%);'>O computador está disponível</p>";
         }
     }
-}
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -439,8 +421,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
     
   
-<form action="" method="POST" enctype="multipart/form-data">
-
+<form action="" method="POST">
     <label class="custum-file-upload" for="file">
         <div class="icon">
         <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24"><g stroke-width="0" id="SVGRepo_bgCarrier"></g><g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" clip-rule="evenodd" fill-rule="evenodd"></path> </g></svg>
@@ -448,17 +429,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="text">
            <span>Click to upload image</span>
            </div>
-           <input type="file" id="file" name="file required">
+           <input type="file" id="file">
     </label>
-    <button type="submit" >Submit</button>
 </form>
-
-    <?php if (!empty($mensagem)): ?>
-    <p><?php echo $mensagem; ?></p>
-    <?php if (isset($imageData)): ?>
-        <img src="data:image/jpeg;base64,<?php echo $imageData; ?>" alt="Uploaded Image" style="max-width: 200px; max-height: 200px;">
-    <?php endif; ?>
-<?php endif; ?>
 
     
     
