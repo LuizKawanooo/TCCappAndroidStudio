b8f78557dc0a760b6926eee768f0d0e86cbd4653
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-09-30 14:32:30 -0300

    Update computadores.php

--- sitebibliotec2/sitetcc/computadores.php
+++ sitebibliotec2/sitetcc/computadores.php
@@ -405,17 +405,68 @@ foreach ($imagens as $imagem) {
 
 
 
-    
-    <h1>Upload de Imagem</h1>
-    <br>
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<style>
+            .upload-button {
+            display: inline-flex;
+            align-items: center;
+            padding: 10px 20px;
+            background-color: #4CAF50; /* Cor do fundo do botão */
+            color: white; /* Cor do texto */
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            transition: background-color 0.3s;
+            text-decoration: none; /* Remove sublinhado */
+        }
 
-    <br>
-    <label for="imagem">Escolha uma imagem:</label>
-    <input type="file" name="imagem" id="imagem" required>
-    <br>
-    <input type="submit" name="upload" value="Enviar">
-</form>
+        .upload-button:hover {
+            background-color: #45a049; /* Cor do botão ao passar o mouse */
+        }
+
+        .upload-button svg {
+            margin-right: 8px; /* Espaçamento entre a SVG e o texto */
+            width: 24px; /* Largura da SVG */
+            height: 24px; /* Altura da SVG */
+        }
+</style>
+
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="imagem">Escolha uma imagem:</label>
+        <input type="file" name="imagem" id="imagem" required>
+        <br>
+        <button type="submit" class="upload-button">
+            <svg
+                aria-hidden="true"
+                stroke="currentColor"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-width="2"
+                    stroke="#ffffff"
+                    d="M13.5 3H12H8C6.34315 3 5 4.34315 5 6V18C5 19.6569 6.34315 21 8 21H11M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V11.8125"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                ></path>
+                <path
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="2"
+                    stroke="#ffffff"
+                    d="M17 15V18M17 21V18M17 18H14M17 18H20"
+                ></path>
+            </svg>
+            Enviar
+        </button>
+    </form>
+
+
+
+
+
 
 
 
