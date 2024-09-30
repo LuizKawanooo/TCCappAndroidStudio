820779d3fbd3c5e90242e9ae0d1502c2e283779a
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-09-30 15:22:16 -0300

    Update computadores.php

--- sitebibliotec2/sitetcc/computadores.php
+++ sitebibliotec2/sitetcc/computadores.php
@@ -433,7 +433,7 @@ foreach ($imagens as $imagem) {
     
 
     <br>
-<form style="margin-left: 150px;" action="upload.php" method="post" enctype="multipart/form-data">
+<form style="margin-left: 660px;" action="upload.php" method="post" enctype="multipart/form-data">
 
     <br>
     <label for="imagem">Escolha uma imagem:</label>
