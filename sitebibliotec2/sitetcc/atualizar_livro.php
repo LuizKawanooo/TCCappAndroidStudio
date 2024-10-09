17f244f4382b76a7bcee1f69da4ac4b48921e8f2
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-10-09 17:42:57 -0300

    Create atualizar_livro.php

---
+++ sitebibliotec2/sitetcc/atualizar_livro.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "tccappionic-bd.mysql.uhserver.com";
+$username = "ionic_perfil_bd";
+$password = "{[UOLluiz2019";
+$dbname = "tccappionic_bd";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+
+$id = isset($_POST['id']) ? intval($_POST['id']) : null;
+$titulo = $_POST['titulo'];
+$autor = $_POST['autor'];
+$editora = $_POST['editora'];
+$genero = $_POST['genero'];
+$tombo = $_POST['tombo'];
+$ano = $_POST['ano'];
+$classificacao = $_POST['classificacao'];
+$n_paginas = $_POST['n_paginas'];
+$isbn = $_POST['isbn'];
+
+$sql = "UPDATE livros SET titulo=?, autor=?, editora=?, genero=?, tombo=?, ano=?, classificacao=?, n_paginas=?, isbn=? WHERE id=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssiissii", $titulo, $autor, $editora, $genero, $tombo, $ano, $classificacao, $n_paginas, $isbn, $id);
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
+
+header("Location: livros.php"); // Redireciona de volta para a lista de livros
+exit();
+?>
