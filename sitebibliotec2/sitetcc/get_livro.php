19ea4a128595d791010e4b7dc4dd60a6128c116d
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-10-09 14:55:29 -0300

    Update get_livro.php

--- sitebibliotec2/sitetcc/get_livro.php
+++ sitebibliotec2/sitetcc/get_livro.php
@@ -1,40 +1,6 @@
-// <?php
-// header('Content-Type: application/json');
-
-// $servername = "tccappionic-bd.mysql.uhserver.com";
-// $username = "ionic_perfil_bd";
-// $password = "{[UOLluiz2019";
-// $dbname = "tccappionic_bd";
-
-// // Cria a conexão
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Verifica a conexão
-// if ($conn->connect_error) {
-//     die("Erro na conexão: " . $conn->connect_error);
-// }
-
-// $id = $_GET['id'];
-
-// $sql = "SELECT * FROM livros WHERE id = ?";
-// $stmt = $conn->prepare($sql);
-// $stmt->bind_param("i", $id);
-// $stmt->execute();
-// $result = $stmt->get_result();
-
-// if ($result->num_rows > 0) {
-//     echo json_encode($result->fetch_assoc());
-// } else {
-//     echo json_encode([]);
-// }
-
-// $stmt->close();
-// $conn->close();
-// ?>
-
-
 <?php
-// Conexão com o banco de dados
+header('Content-Type: application/json');
+
 $servername = "tccappionic-bd.mysql.uhserver.com";
 $username = "ionic_perfil_bd";
 $password = "{[UOLluiz2019";
@@ -48,23 +14,20 @@ if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
 
-// Verifica se o ID do livro foi passado
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    $sql = "SELECT * FROM livros WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
+$id = $_GET['id'];
 
-    if ($result->num_rows > 0) {
-        echo json_encode($result->fetch_assoc());
-    } else {
-        echo json_encode([]);
-    }
+$sql = "SELECT * FROM livros WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo json_encode($result->fetch_assoc());
 } else {
     echo json_encode([]);
 }
 
+$stmt->close();
 $conn->close();
 ?>
