3577536b512874bfc112d222039694f7f400e6fe
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-10-11 23:45:56 -0300

    Update get_livro.php

--- sitebibliotec2/sitetcc/get_livro.php
+++ sitebibliotec2/sitetcc/get_livro.php
@@ -186,6 +186,17 @@ $conn->close();
             window.location.href = "https://endologic.com.br/tcc/sitebibliotec2/sitetcc/livros.php";
         }
     </script>
+
+
+    <div class="footer">
+        <svg viewBox="0 0 869 344" xmlns="http://www.w3.org/2000/svg">
+            <path d="M 272 0.0130308C 164.8 1.21303 46 85.1797 0 127.013L 0 342.013L 867 342.013L 867 6.51303C 779 0.013031 684.5 127.013 616.5 127.013C 548.5 127.013 406 -1.48697 272 0.0130308Z"/>
+        </svg>
+    </div>
+
+
+
+    
 </body>
 </html>
 
