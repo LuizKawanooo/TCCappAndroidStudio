44f8275e22dc0b05be2776b005dd065131b048b6
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-10-16 14:40:08 -0300

    Update upload_tcc.php

--- sitebibliotec2/sitetcc/upload_tcc.php
+++ sitebibliotec2/sitetcc/upload_tcc.php
@@ -1,60 +1,3 @@
-// <?php
-// $servername = "tccappionic-bd.mysql.uhserver.com";
-// $username = "ionic_perfil_bd";
-// $password = "{[UOLluiz2019";
-// $dbname = "tccappionic_bd";
-
-// // Cria a conexão
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Verifica a conexão
-// if ($conn->connect_error) {
-//     die("Erro na conexão: " . $conn->connect_error);
-// }
-
-// $id = $_POST['id'];
-// $titulo = $_POST['titulo'];
-// $autor = $_POST['autor'];
-// $ano = $_POST['ano'];
-// $arquivo = null;
-// // faz upload de arquivos
-// if (isset($_FILES['arquivo']) && $_FILES['arquivo']['error'] === UPLOAD_ERR_OK) {
-//     $arquivo = file_get_contents($_FILES['arquivo']['tmp_name']);
-// }
-
-// if ($arquivo) {
-//     $sql = "UPDATE artigos SET titulo='$titulo', autor='$autor', ano='$ano', arquivo='$arquivo' WHERE id=$id";
-// } else {
-//     $sql = "INSERT INTO artigos SET titulo='$titulo', autor='$autor', ano='$ano' WHERE id=$id";
-// }
-
-// if ($conn->query($sql) === TRUE) {
-//     echo "Artigo atualizado com sucesso";
-// } else {
-//     echo "Erro ao atualizar artigo: " . $conn->error;
-// }
-
-// $conn->close();
-// //recarrega a página
-// header('Location: tcc.php');
-// ?>
-                     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
 // Conexão com o banco de dados
 $servername = "tccappionic-bd.mysql.uhserver.com";
@@ -106,4 +49,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id'])) {
 
 $conn->close();
 ?>
-
