cfb66aa25a6ffce615c951c2eb41cd271aadaaba
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-10-16 13:03:12 -0300

    Update livros.php

--- sitebibliotec2/sitetcc/livros.php
+++ sitebibliotec2/sitetcc/livros.php
@@ -527,7 +527,7 @@
     <div class="gen">
         <h1>GÊNEROS</h1>
         <a style="color: white; text-decoration: none;" href="?search=" class="btn-todos"><div class="generos" data-genero="Infantil">Infantil</div></a>
-        <div class="generos" data-genero="Infantil">Infantil</div>
+        <div class="generos" data-genero="Todos">Todos</div>
         <div class="generos" data-genero="Ficção">Ficção</div>
         <div class="generos" data-genero="Romance">Romance</div>
         <div class="generos" data-genero="Drama">Drama</div>
