98ae819d4e677dcfe16d84dac1dc5ff6d1ed4e1f
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2024-11-01 12:05:41 -0300

    Update get_image.php

--- get_image.php
+++ get_image.php
@@ -1,4 +1,11 @@
 <?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+
 header("Content-Type: image/jpeg");
 include 'database_connection.php';
 
