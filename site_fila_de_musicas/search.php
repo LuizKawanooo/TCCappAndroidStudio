1b9e1ce4af0d3c748e3d89e4c2988a843d31901c
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2025-01-17 13:38:15 -0300

    Update search.php

--- site_fila_de_musicas/search.php
+++ site_fila_de_musicas/search.php
@@ -6,13 +6,27 @@ $query = urlencode($_GET['query']);
 $maxResults = 5;
 $apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={$query}&maxResults={$maxResults}&key={$apiKey}";
 
-$response = @file_get_contents($apiUrl);
+// Inicializa a sessão cURL
+$ch = curl_init();
 
-if ($response === FALSE) {
-    echo 'Não foi possível completar a solicitação. Verifique a chave de API e as configurações.';
+// Configurações da requisição cURL
+curl_setopt($ch, CURLOPT_URL, $apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Executa a requisição e obtém a resposta
+$response = curl_exec($ch);
+
+// Verifica se houve erro na requisição
+if ($response === false) {
+    echo 'Erro na requisição: ' . curl_error($ch);
+    curl_close($ch);
     exit;
 }
 
+// Fecha a sessão cURL
+curl_close($ch);
+
+// Decodifica a resposta JSON
 $results = json_decode($response, true);
 
 echo '<h1>Resultados da Pesquisa</h1>';
@@ -29,5 +43,5 @@ if (isset($results['items']) && is_array($results['items'])) {
     echo 'Nenhum resultado encontrado ou ocorreu um erro na solicitação.';
 }
 
-echo '<a href="player.html">Ir para o Player</a>';
+echo '<a href="player.html'>Ir para o Player</a>';
 ?>
