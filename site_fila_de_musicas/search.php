75ac6d91a3f8f0d5291e35611b0e8b31cc16b569
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2025-01-17 13:31:52 -0300

    Update search.php

--- site_fila_de_musicas/search.php
+++ site_fila_de_musicas/search.php
@@ -28,7 +28,7 @@
 <?php
 header("Access-Control-Allow-Origin: *"); // Permite requisições de qualquer origem
 
-$apiKey = 'SUA_CHAVE_DE_API_DO_YOUTUBE';
+$apiKey = 'AIzaSyB5OpyEuIekeMW6zLr8DdE0qbnJPD5OfRU';
 $query = urlencode($_GET['query']);
 $maxResults = 5;
 $apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={$query}&maxResults={$maxResults}&key={$apiKey}";
