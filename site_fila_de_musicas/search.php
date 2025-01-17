6c0bae81e71ca83093f2ad2d1aa54137a4200536
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2025-01-17 13:19:40 -0300

    Update and rename fetch_queue.php to search.php

---
+++ site_fila_de_musicas/search.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Permite requisições de qualquer origem
+
+$apiKey = 'SUA_CHAVE_DE_API_DO_YOUTUBE';
+$query = urlencode($_GET['query']);
+$maxResults = 5;
+$apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={$query}&maxResults={$maxResults}&key={$apiKey}";
+
+$response = file_get_contents($apiUrl);
+$results = json_decode($response, true);
+
+echo '<h1>Resultados da Pesquisa</h1>';
+echo '<ul>';
+foreach ($results['items'] as $item) {
+    $videoId = $item['id']['videoId'];
+    $title = $item['snippet']['title'];
+    echo "<li>{$title} <a href='queue.php?videoId={$videoId}&title=" . urlencode($title) . "'>Adicionar à Fila</a></li>";
+}
+echo '</ul>';
+echo '<a href="player.html">Ir para o Player</a>';
+?>
