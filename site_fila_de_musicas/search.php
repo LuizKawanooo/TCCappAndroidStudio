871b8e08fcee4e2b7009fbe562fd7364d04ae99d
PomodoroTimer <107799683+LuizKawanooo@users.noreply.github.com>
2025-01-17 13:31:38 -0300

    Update search.php

--- site_fila_de_musicas/search.php
+++ site_fila_de_musicas/search.php
@@ -1,7 +1,34 @@
+// <?php
+// header("Access-Control-Allow-Origin: *"); // Permite requisições de qualquer origem
+
+// $apiKey = 'AIzaSyB5OpyEuIekeMW6zLr8DdE0qbnJPD5OfRU';
+// $query = urlencode($_GET['query']);
+// $maxResults = 5;
+// $apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={$query}&maxResults={$maxResults}&key={$apiKey}";
+
+// $response = file_get_contents($apiUrl);
+// $results = json_decode($response, true);
+
+// echo '<h1>Resultados da Pesquisa</h1>';
+// echo '<ul>';
+// foreach ($results['items'] as $item) {
+//     $videoId = $item['id']['videoId'];
+//     $title = $item['snippet']['title'];
+//     echo "<li>{$title} <a href='queue.php?videoId={$videoId}&title=" . urlencode($title) . "'>Adicionar à Fila</a></li>";
+// }
+// echo '</ul>';
+// echo '<a href="player.html">Ir para o Player</a>';
+// ?>
+
+
+
+
+
+
 <?php
 header("Access-Control-Allow-Origin: *"); // Permite requisições de qualquer origem
 
-$apiKey = 'AIzaSyB5OpyEuIekeMW6zLr8DdE0qbnJPD5OfRU';
+$apiKey = 'SUA_CHAVE_DE_API_DO_YOUTUBE';
 $query = urlencode($_GET['query']);
 $maxResults = 5;
 $apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q={$query}&maxResults={$maxResults}&key={$apiKey}";
@@ -10,12 +37,18 @@ $response = file_get_contents($apiUrl);
 $results = json_decode($response, true);
 
 echo '<h1>Resultados da Pesquisa</h1>';
-echo '<ul>';
-foreach ($results['items'] as $item) {
-    $videoId = $item['id']['videoId'];
-    $title = $item['snippet']['title'];
-    echo "<li>{$title} <a href='queue.php?videoId={$videoId}&title=" . urlencode($title) . "'>Adicionar à Fila</a></li>";
+
+if (isset($results['items']) && is_array($results['items'])) {
+    echo '<ul>';
+    foreach ($results['items'] as $item) {
+        $videoId = $item['id']['videoId'];
+        $title = $item['snippet']['title'];
+        echo "<li>{$title} <a href='queue.php?videoId={$videoId}&title=" . urlencode($title) . "'>Adicionar à Fila</a></li>";
+    }
+    echo '</ul>';
+} else {
+    echo 'Nenhum resultado encontrado ou ocorreu um erro na solicitação.';
 }
-echo '</ul>';
+
 echo '<a href="player.html">Ir para o Player</a>';
 ?>
